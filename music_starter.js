d72ed32e56531b696163a9bb3918d5242d074b9e
AstonRobbins <aston.robbins08@gmail.com>
2024-08-08 17:03:13 +1200

    first Push

--- music_starter.js
+++ music_starter.js
@@ -1,11 +1,84 @@
 
+class Rotation{
+  rotate;
+}
+
+r = new Rotation;
+r.rotate = 0;
+
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+
+  vocalX = map(vocal, 0, 100, 0, 540);
+  vocalY = map(vocal, 0, 100, 0, 960);
+
+  drumX = map(drum, 0 , 100, 0, 540);
+  drumY = map(drum, 0 , 100, 0, 960);
+
+  bassX = map(bass, 0 ,100, 0, 540);
+  bassY = map(bass, 0, 100, 0, 960);
+
+  otherX = map(other, 0, 100, 0, 540);
+  otherY = map(other, 0, 100, 0, 960);
+
+  if(vocal < 30){vocalX = 0;}
+  if(drum < 30){drumX = 0;}
+  if(bass < 30){bassX = 0;}
+  if(other < 30){otherX = 0;}
+
+  controlPoints1 = [
+    { x: 0, y: 0 },
+    { x: vocalX, y: 0 },
+    { x: 540 - vocalX, y: 960 },
+    { x: 540, y: 960 }
+  ];
+  controlPoints2 = [
+    { x: 540, y: 0 },
+    { x: 540 - drumX, y: 0 },
+    { x: drumX, y: 960 },
+    { x: 0, y: 960 }
+  ];
+  controlPoints3 = [
+    { x: 0, y: 0 },
+    { x: - bassX, y: 0 },
+    { x: 540 + bassX, y: 960 },
+    { x: 540, y: 960 }
+  ];
+  controlPoints4 = [
+    { x: 540, y: 0 },
+    { x: 540 + otherX, y: 0 },
+    { x: - otherX, y: 960 },
+    { x: 0, y: 960 }
+  ];
+
+  fill(110,30,100,250);
+  bezier(controlPoints1[0].x,controlPoints1[0].y,controlPoints1[1].x,controlPoints1[1].y,
+         controlPoints1[2].x,controlPoints1[2].y,controlPoints1[3].x,controlPoints1[3].y
+  );
+
+  fill(20,80,180);
+  bezier(controlPoints2[0].x,controlPoints2[0].y,controlPoints2[1].x,controlPoints2[1].y,
+    controlPoints2[2].x,controlPoints2[2].y,controlPoints2[3].x,controlPoints2[3].y
+  );
+
+  fill(80,180,60);
+  bezier(controlPoints3[0].x,controlPoints3[0].y,controlPoints3[1].x,controlPoints3[1].y,
+         controlPoints3[2].x,controlPoints3[2].y,controlPoints3[3].x,controlPoints3[3].y
+  );
+
+  fill(150,20,10);
+  bezier(controlPoints4[0].x,controlPoints4[0].y,controlPoints4[1].x,controlPoints4[1].y,
+    controlPoints4[2].x,controlPoints4[2].y,controlPoints4[3].x,controlPoints4[3].y
+  );
   
+
+
+  /*
+
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
@@ -40,4 +113,5 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    textAlign(CENTER);
    textSize(vocal);
    text(words, width/2, height/3);
+   */
 }
