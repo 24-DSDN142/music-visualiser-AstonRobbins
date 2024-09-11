fb6ee1678c631d633522855daaccc1a6bb72b31f
robbinasto <robbinasto@ecs.vuw.ac.nz>
2024-09-12 03:43:29 +1200

    code cleanup

--- music_starter.js
+++ music_starter.js
@@ -456,9 +456,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   drawGradientBackground();
 
-  //fill(6,21,71);
-  //rect(cWidth/2,cHeight/2,bigScreenWidth,bigScreenHeight); //big screen
-  drawOscillatingGradient();
+  drawOscillatingGradient(); //big screen
   fill(20);
   rect(cWidth/7,cHeight/2,smallScreenWidth,smallScreenHeight); //little screen left
   rect(cWidth - cWidth/7, cHeight/2,smallScreenWidth,smallScreenHeight); //little screen right
@@ -479,18 +477,11 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     vocal = map(vocal, 50, 100, 0, 100); //remap vocal volume to 50,100 for smoother colour transition
   }
 
-  turnOnBulbsInCirclePattern(25, 50, vocal, cWidth / 7, bass); //left screen
-  turnOnBulbsInCirclePattern(25, 50, vocal, cWidth - cWidth / 7, bass); //right screen
-
-  startDiagonalLineAnimation(25, 50, vocal, bass);
+  turnOnBulbsInCirclePattern(25, 50, vocal, cWidth / 7, bass); //left screen bulbs circles
+  turnOnBulbsInCirclePattern(25, 50, vocal, cWidth - cWidth / 7, bass); //right screen bulbs circles
+  startDiagonalLineAnimation(25, 50, vocal, bass); //both small screen bulbs lines
 
   rectMode(CENTER)
-  //show words on small screens
-  fill(20,180,240);
-  textAlign(CENTER, CENTER);
-  //text can't be shown due to song change not containing words
-  //text(words, cWidth / 7, cHeight / 2); //display on left screen
-  //text(words, cWidth - cWidth / 7, cHeight / 2); //display on right screen
 
   drawLasers();
 
@@ -527,124 +518,4 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   updateGradient(bass); //update gradient based on bass
   updateLaserGlowIntensity(drum); // update lasers based on drum
-
-  /*
-  quad(
-   cWidth/8, cHeight/2 - smallScreenHeight/2, //top left
-   cWidth/8 + smallScreenWidth, cHeight/2 - smallScreenHeight/2, //top right
-   cWidth/8 + smallScreenWidth, cHeight/2 + smallScreenHeight/2, //bottom right
-   cWidth/8, cHeight/2 + smallScreenHeight/2, //bottom left
-  );
-
-  quad(
-    cWidth/4 * 2.5, cHeight/2 - smallScreenHeight/2, //top left
-    cWidth/4 * 2.5+ smallScreenWidth, cHeight/2 - smallScreenHeight/2, //top right
-    cWidth/4 * 2.5+ smallScreenWidth, cHeight/2 + smallScreenHeight/2, //bottom right
-    cWidth/4 * 2.5, cHeight/2 + smallScreenHeight/2, //bottom left
-   );
-
-   */
-
-  /*
-  vocalX = map(vocal, 0, 100, 0, 540);
-  vocalY = map(vocal, 0, 100, 0, 960);
-
-  drumX = map(drum, 0 , 100, 0, 540);
-  drumY = map(drum, 0 , 100, 0, 960);
-
-  bassX = map(bass, 0 ,100, 0, 540);
-  bassY = map(bass, 0, 100, 0, 960);
-
-  otherX = map(other, 0, 100, 0, 540);
-  otherY = map(other, 0, 100, 0, 960);
-
-  if(vocal < 30){vocalX = 0;}
-  if(drum < 30){drumX = 0;}
-  if(bass < 30){bassX = 0;}
-  if(other < 30){otherX = 0;}
-
-  controlPoints1 = [
-    { x: 0, y: 0 },
-    { x: vocalX, y: 0 },
-    { x: 540 - vocalX, y: 960 },
-    { x: 540, y: 960 }
-  ];
-  controlPoints2 = [
-    { x: 540, y: 0 },
-    { x: 540 - drumX, y: 0 },
-    { x: drumX, y: 960 },
-    { x: 0, y: 960 }
-  ];
-  controlPoints3 = [
-    { x: 0, y: 0 },
-    { x: - bassX, y: 0 },
-    { x: 540 + bassX, y: 960 },
-    { x: 540, y: 960 }
-  ];
-  controlPoints4 = [
-    { x: 540, y: 0 },
-    { x: 540 + otherX, y: 0 },
-    { x: - otherX, y: 960 },
-    { x: 0, y: 960 }
-  ];
-
-  fill(110,30,100,250);
-  bezier(controlPoints1[0].x,controlPoints1[0].y,controlPoints1[1].x,controlPoints1[1].y,
-         controlPoints1[2].x,controlPoints1[2].y,controlPoints1[3].x,controlPoints1[3].y
-  );
-
-  fill(20,80,180);
-  bezier(controlPoints2[0].x,controlPoints2[0].y,controlPoints2[1].x,controlPoints2[1].y,
-    controlPoints2[2].x,controlPoints2[2].y,controlPoints2[3].x,controlPoints2[3].y
-  );
-
-  fill(80,180,60);
-  bezier(controlPoints3[0].x,controlPoints3[0].y,controlPoints3[1].x,controlPoints3[1].y,
-         controlPoints3[2].x,controlPoints3[2].y,controlPoints3[3].x,controlPoints3[3].y
-  );
-
-  fill(150,20,10);
-  bezier(controlPoints4[0].x,controlPoints4[0].y,controlPoints4[1].x,controlPoints4[1].y,
-    controlPoints4[2].x,controlPoints4[2].y,controlPoints4[3].x,controlPoints4[3].y
-  );
-  */
-
-
-  /*
-
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-   */
 }
